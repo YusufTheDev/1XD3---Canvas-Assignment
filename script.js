5197325242a201c538b2bc853b9faa018267deae
Yusuf Khan <yusuf.k2606@gmail.com>
2025-03-04 17:13:42 -0500

    lab 8.1 changes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.addEventListener('load', function() {
 
     class Square {
         constructor(length, red, green, blue, x, y) {
-            this.length = length;
+            this.length = length * 2;
             this.red = red;
             this.green = green;
             this.blue = blue;
@@ -30,13 +30,13 @@ window.addEventListener('load', function() {
         
         draw(ctx) {
             ctx.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
-            ctx.fillRect(this.x, this.y, this.length, this.length);
+            ctx.fillRect(this.x - this.length / 2, this.y - this.length / 2, this.length, this.length);
         }
     }
 
     class Triangle {
         constructor(length, red, green, blue, x, y) {
-            this.length = length;
+            this.length = length * 2;
             this.red = red;
             this.green = green;
             this.blue = blue;
@@ -47,23 +47,59 @@ window.addEventListener('load', function() {
         draw(ctx) {
             ctx.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
             ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(this.x + this.length, this.y);
-            ctx.lineTo(this.x + this.length / 2, this.y + this.length);
+            ctx.moveTo(this.x, this.y - this.length / 2); // Top vertex
+            ctx.lineTo(this.x - this.length / 2, this.y + this.length / 2); // Bottom left vertex
+            ctx.lineTo(this.x + this.length / 2, this.y + this.length / 2); // Bottom right vertex
+            ctx.closePath();
             ctx.fill();
         }
     }
     
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
+    let objects = [];
 
     const shapeSelect = document.getElementById('shape');
     const sizeInput = document.getElementById('size');
     const colorInput = document.getElementById('color');
     const helpButton = document.getElementById('helpButton');
+    const clearButton = document.getElementById('clearButton');
+    const undoButton = document.getElementById('undoButton');
+
+    // Load objects from local storage
+    if (localStorage.getItem('objects')) {
+        objects = JSON.parse(localStorage.getItem('objects')).map(obj => {
+            switch (obj.type) {
+                case 'circle':
+                    return new Circle(obj.radius, obj.red, obj.green, obj.blue, obj.x, obj.y);
+                case 'square':
+                    return new Square(obj.length, obj.red, obj.green, obj.blue, obj.x, obj.y);
+                case 'triangle':
+                    return new Triangle(obj.length, obj.red, obj.green, obj.blue, obj.x, obj.y);
+            }
+        });
+        redrawCanvas();
+    }
 
     helpButton.addEventListener('click', function() {
-        alert('To draw a shape, select the shape type, size, and color, then click on the canvas.');
+        let helpText = document.getElementById("helptext");
+        if (helpText.style.display === "none" || helpText.style.display === "") {
+            helpText.style.display = "block";
+        } else {
+            helpText.style.display = "none";
+        }
+    });
+
+    clearButton.addEventListener('click', function() {
+        objects = [];
+        saveObjects();
+        redrawCanvas();
+    });
+
+    undoButton.addEventListener('click', function() {
+        objects.pop();
+        saveObjects();
+        redrawCanvas();
     });
 
     canvas.addEventListener('click', function(e) {
@@ -89,7 +125,26 @@ window.addEventListener('load', function() {
                 break;
         }
         if (shape) {
-            shape.draw(ctx);
+            objects.push(shape);
+            saveObjects();
+            redrawCanvas();
         }
     });
+
+    function redrawCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        objects.forEach(shape => shape.draw(ctx));
+    }
+
+    function saveObjects() {
+        localStorage.setItem('objects', JSON.stringify(objects.map(obj => {
+            if (obj instanceof Circle) {
+                return { type: 'circle', radius: obj.radius, red: obj.red, green: obj.green, blue: obj.blue, x: obj.x, y: obj.y };
+            } else if (obj instanceof Square) {
+                return { type: 'square', length: obj.length, red: obj.red, green: obj.green, blue: obj.blue, x: obj.x, y: obj.y };
+            } else if (obj instanceof Triangle) {
+                return { type: 'triangle', length: obj.length, red: obj.red, green: obj.green, blue: obj.blue, x: obj.x, y: obj.y };
+            }
+        })));
+    }
 });
